dbe7e1985f27916c46c1280be639a3b9167e75d8
Christian Banse <oxisto@aybaze.com>
2021-05-13 22:40:38 +0200

    Consistent naming scheme for GitHub release assets

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@ go mod download
 
 if [ "$1" = "--ci" ]; then
     # cross-compilation for CI
-    GOOS=linux GOARCH=amd64 go build -o okr2go-linux-amd64 cmd/okr2go/okr2go.go
-    GOOS=darwin GOARCH=arm64 go build -o okr2go-darwin-arm64 cmd/okr2go/okr2go.go
-    GOOS=darwin GOARCH=amd64 go build -o okr2go-darwin-amd64 cmd/okr2go/okr2go.go
-    GOOS=windows GOARCH=amd64 go build -o okr2go-windows-amd64.exe cmd/okr2go/okr2go.go
-    go build cmd/okr2go/okr2go.go
+    GOOS=linux GOARCH=amd64 go build cmd/okr2go/okr2go.go && zip okr2go-linux-x86_64.zip okr2go
+    GOOS=darwin GOARCH=arm64 go build cmd/okr2go/okr2go.go && zip okr2go-macos-aarch_64.zip okr2go
+    GOOS=darwin GOARCH=amd64 go build cmd/okr2go/okr2go.go && zip okr2go-macos-x86_64.zip okr2go
+    GOOS=windows GOARCH=amd64 go build cmd/okr2go/okr2go.go && zip okr2go-win64.zip okr2go.exe
 fi
+
+go build cmd/okr2go/okr2go.go
 
 # install for local
 go install cmd/okr2go/okr2go.go
