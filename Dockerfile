cf510423a89f02c3eee0f5d33f27509babb280ab
Christian Banse <oxisto@aybaze.com>
2021-05-14 15:55:17 +0200

    Moving artifacts into build folder

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,7 @@ FROM debian:buster-slim
 ARG TARGETOS
 ARG TARGETARCH
 
-ADD okr2go .
-
-ADD okr2go-${TARGETOS}-${TARGETARCH}.tar.gz .
+ADD build/okr2go-${TARGETOS}-${TARGETARCH}.tar.gz .
 RUN ls *
 
 ENTRYPOINT [ "okr2go" ]
--- build.sh
+++ build.sh
@@ -4,12 +4,14 @@ cd ui && yarn install --ignore-optional && yarn build && cd ..
 go mod download
 
 if [ "$1" = "--ci" ]; then
+    mkdir -p build
+
     # cross-compilation for CI
-    GOOS=linux GOARCH=arm64 go build cmd/okr2go/okr2go.go && tar -czf okr2go-linux-arm64.tar.gz okr2go
-    GOOS=linux GOARCH=amd64 go build cmd/okr2go/okr2go.go && tar -czf okr2go-linux-amd64.tar.gz okr2go
-    GOOS=darwin GOARCH=arm64 go build cmd/okr2go/okr2go.go && tar -czf okr2go-darwin-arm64.tar.gz okr2go
-    GOOS=darwin GOARCH=amd64 go build cmd/okr2go/okr2go.go && tar -czf okr2go-darwin-amd64.tar.gz okr2go
-    GOOS=windows GOARCH=amd64 go build cmd/okr2go/okr2go.go && tar -czf okr2go-windows-amd64.tar.gz okr2go.exe
+    GOOS=linux GOARCH=arm64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-linux-arm64.tar.gz okr2go
+    GOOS=linux GOARCH=amd64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-linux-amd64.tar.gz okr2go
+    GOOS=darwin GOARCH=arm64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-darwin-arm64.tar.gz okr2go
+    GOOS=darwin GOARCH=amd64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-darwin-amd64.tar.gz okr2go
+    GOOS=windows GOARCH=amd64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-windows-amd64.tar.gz okr2go.exe
 fi
 
 go build cmd/okr2go/okr2go.go
