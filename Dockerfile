5d7e42286ef6cd37851af6b6bae5bbc1beb78e88
Christian Banse <oxisto@aybaze.com>
2021-05-14 16:52:58 +0200

    Added Docker BuildX support (#32)
    * Added Docker BuildX support
    * More consistent naming of GH assets... again
    * Building a multiarch Docker image
    * Using tar.gz instead of zip for easier Dockerfile integration
    * Moving artifacts into build folder
    * Added push to GitHub registry
    * Added node cache and support for QEMU on buildx
    * Some last CI optimizations

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM debian:buster-slim
+
+ARG TARGETOS
+ARG TARGETARCH
+
+ADD build/okr2go-${TARGETOS}-${TARGETARCH}.tar.gz .
+
+EXPOSE 4300
+
+ENTRYPOINT [ "okr2go" ]
--- build.sh
+++ build.sh
@@ -4,11 +4,14 @@ cd ui && yarn install --ignore-optional && yarn build && cd ..
 go mod download
 
 if [ "$1" = "--ci" ]; then
+    mkdir -p build
+
     # cross-compilation for CI
-    GOOS=linux GOARCH=amd64 go build cmd/okr2go/okr2go.go && zip okr2go-linux-x86_64.zip okr2go
-    GOOS=darwin GOARCH=arm64 go build cmd/okr2go/okr2go.go && zip okr2go-macos-aarch_64.zip okr2go
-    GOOS=darwin GOARCH=amd64 go build cmd/okr2go/okr2go.go && zip okr2go-macos-x86_64.zip okr2go
-    GOOS=windows GOARCH=amd64 go build cmd/okr2go/okr2go.go && zip okr2go-win64.zip okr2go.exe
+    GOOS=linux GOARCH=arm64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-linux-arm64.tar.gz okr2go
+    GOOS=linux GOARCH=amd64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-linux-amd64.tar.gz okr2go
+    GOOS=darwin GOARCH=arm64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-darwin-arm64.tar.gz okr2go
+    GOOS=darwin GOARCH=amd64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-darwin-amd64.tar.gz okr2go
+    GOOS=windows GOARCH=amd64 go build cmd/okr2go/okr2go.go && tar -czf build/okr2go-windows-amd64.tar.gz okr2go.exe
 fi
 
 go build cmd/okr2go/okr2go.go
